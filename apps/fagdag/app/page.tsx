4615bda1ef083a5452a74199189dc22e1bd01b8b
fredrik neby <fredneby@gmail.com>
2023-10-31 11:56:52 +0100

    added env message

--- apps/fagdag/app/page.tsx
+++ apps/fagdag/app/page.tsx
@@ -16,6 +16,22 @@ export default async function Index() {
               Welcome fagdag 👋
             </h1>
             <p>This is a test site for personal learning</p>
+            <div style={{ display: 'flex', marginTop: '10px' }}>
+              <span>Current environment: </span>
+              <span
+                className="environment"
+                style={{
+                  paddingLeft: '5px',
+                  paddingRight: '5px',
+                  marginLeft: '5px',
+                  backgroundColor: 'green',
+                  borderRadius: '2px',
+                  color: 'white',
+                }}
+              >
+                {process.env.WHAT_ENVIRONMENT_AM_I}
+              </span>
+            </div>
           </div>
 
           <div id="hero" className="rounded">
